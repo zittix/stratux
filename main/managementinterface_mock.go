0da318a9a11fe55b226dd0c15b6061a576d23037
Mathieu Monney <mathieu.monney@xwaves.net>
2018-11-06 20:48:18 +0100

    Remove changes to goflying to make it compiles in OS X

--- main/managementinterface_mock.go
+++ main/managementinterface_mock.go
@@ -3,9 +3,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type FakeSensor struct {
+}
+
+func (s FakeSensor) Close() {
+
+}
+
+var (
+	myIMUReader      *FakeSensor
+	myPressureReader *FakeSensor
+	analysisLogger   *FakeSensor
+)
+
+func initI2CSensors() {
+	myIMUReader = new(FakeSensor)
+	myPressureReader = new(FakeSensor)
+	analysisLogger = new(FakeSensor)
+}
+
+func CalibrateAHRS() {
+
+}
+
+func ResetAHRSGLoad() {
+
+}
+
+func CageAHRS() {
+
+}
+
+func getMinAccelDirection() (i int, err error) {
+	err = fmt.Errorf("Not implemented")
+	i = 0
+	return i, err
+}
+
+func isAHRSInvalidValue(val float64) bool {
+	return true
+}
 
 func handleShutdownRequest(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Shutdown request received")
--- test/bmp180_read.go
+++ test/bmp180_read.go
@@ -1,11 +1,12 @@
+// +build !mock
+
 package main
 
 import (
 	"fmt"
 
 	"github.com/kidoman/embd"
-
-	//_ "github.com/kidoman/embd/host/all"
+	_ "github.com/kidoman/embd/host/all"
 	"github.com/kidoman/embd/sensor/bmp180"
 )
 
